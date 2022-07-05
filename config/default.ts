26ac6a150c4b4befede94a6e14bc1a22bdc6b1ac
Lukasz Mederak <lukasz.mederak@gmail.com>
2022-07-05 18:54:54 +0200

    Change config production file

--- config/default.ts
+++ config/default.ts
@@ -11,8 +11,4 @@ export default {
   refreshTokenPublicKey: ``,
   stripeSecret: ``,
   stripeWebhookSecret: ``,
-  // googleClientId:
-  //   '769866122856-jdklv80a00phl9s8t5qoqvsusupgm5mk.apps.googleusercontent.com',
-  // googleClientSecret: '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',
-  // googleOauthRedirectUrl: 'http://localhost:1337/api/v1/sessions/oauth/google',
 };
