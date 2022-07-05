2ed6673388255e10059a00d9e7e91dec5b440422
Lukasz Mederak <lukasz.mederak@gmail.com>
2022-07-05 19:05:24 +0200

    Change config production file

---
+++ config/test.ts
@@ -0,0 +1,14 @@
+export default {
+  port: 1337,
+  origin: 'http://localhost:3000',
+  dbUri: ``,
+  saltWorkFactor: 10,
+  accessTokenTtl: '15m',
+  refreshTokenTtl: '1y',
+  accessTokenPrivateKey: ``,
+  accessTokenPublicKey: ``,
+  refreshTokenPrivateKey: ``,
+  refreshTokenPublicKey: ``,
+  stripeSecret: ``,
+  stripeWebhookSecret: ``,
+};
